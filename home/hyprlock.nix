5edc8c22b263f6a0a3da92846d13099a29cde745
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-14 17:11:50 -0600

    Reverting the change

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -19,7 +19,7 @@
       # Set background image
       background = {
         monitor = "";
-        path = "/home/jay/Documents/Backgrounds/britt_kids_hiking_sandiacave.jpg";
+        path = "/home/jay/Documents/Backgrounds/kids_cloudcroft.jpg";
       };
 
       # Input field settings
