abfcd66658dda520480d3c41336355f9ddf9f3de
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-19 02:04:15 -0600

    added tilde

--- home/default.nix
+++ home/default.nix
@@ -94,6 +94,7 @@
     xcur2png # converst cursors to PNG images
     wl-clipboard  # clipboard
     brightnessctl  # screen brightness controller
+    tilde  # command line text editor
 
     # networking
     # samba4Full  # For file sharing between computers on the same network
