83ec3192e9cd52ae73016cea057cc02225e23e19
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-20 00:31:22 -0600

    reverting hyprland

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
     };
     hyprland = {
       # url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-      url = "github:hyprwm/Hyprland";
+      url = "github:hyprwm/Hyprland/v0.41.1?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hy3 = {
-      url = "github:outfoxxed/hy3";
+      url = "github:outfoxxed/hy3/5693ea2";
       inputs.hyprland.follows = "hyprland";
     };
   };
