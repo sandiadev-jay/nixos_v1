1496e27cc8b8041cee9a3a10051b9a4a81682bd1
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-25 23:27:51 -0600

    tlp settings

--- tlp.nix
+++ tlp.nix
@@ -4,6 +4,15 @@
   # Enable TLP for reduced battery consumption
   services.tlp = {
     enable = true;
-    
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORMPROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+    };
   };
 }
