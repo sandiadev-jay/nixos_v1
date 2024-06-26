abc265216af106dde649043fde4e94a784e6c790
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-26 01:24:28 -0600

    Nvidia fine grained power management and offload conversion (trying for more battery life)

--- gpu.nix
+++ gpu.nix
@@ -39,7 +39,7 @@
     powerManagement.enable = true;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -61,7 +61,11 @@
 
     # Laptop required config for dual GPU setup
     prime = {
-      sync.enable = true;
+      # sync.enable = true;
+      offlad = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       # Make sure to use the correct Bus ID values for your system!
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
