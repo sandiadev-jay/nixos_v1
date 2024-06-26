6b6ddaefd67f810eeb25c38f95693c377b431de3
Jay Anderson <jay.anderson@sandiadev.com>
2024-06-26 01:45:20 -0600

    flake fixes

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, nixos-hardware, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
